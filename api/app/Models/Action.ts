d9b77d753eabe41707cec92800236408e5ac2430
Luiz Alex Butkeivicz <luisalexsh@hotmail.com>
2022-01-11 16:33:23 -0300

    omit 'createdAt' and 'updatedAt' by serialization from Action model

--- api/app/Models/Action.ts
+++ api/app/Models/Action.ts
@@ -33,9 +33,9 @@ export default class Action extends BaseModel {
   @column()
   public mpCost: number
 
-  @column.dateTime({ autoCreate: true })
+  @column.dateTime({ autoCreate: true, serializeAs: null })
   public createdAt: DateTime
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  @column.dateTime({ autoCreate: true, autoUpdate: true, serializeAs: null })
   public updatedAt: DateTime
 }
