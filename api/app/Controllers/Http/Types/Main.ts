d147e929f911dabb3d114a29154832326fb0b515
Luiz Alex Butkeivicz <luisalexsh@hotmail.com>
2022-01-11 16:33:45 -0300

    removed 'action' preload from Types controller

--- api/app/Controllers/Http/Types/Main.ts
+++ api/app/Controllers/Http/Types/Main.ts
@@ -3,7 +3,7 @@ import { Type } from 'App/Models'
 
 export default class TypesController {
   public async index({ }: HttpContextContract) {
-    const data = await Type.query().preload('actions')
+    const data = await Type.query()
     return data;
   }
 }
