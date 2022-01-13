fd89247667c35cfa89810195a86491638eeb8129
Luiz Alex Butkeivicz <luisalexsh@hotmail.com>
2022-01-13 16:29:12 -0300

    add 'Aspect' model

---
+++ api/app/Models/Aspect.ts
@@ -0,0 +1,34 @@
+import { BaseModel, column, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
+import { Action } from '.'
+
+export default class Aspect extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public key: string
+
+  @column()
+  public name: string
+
+  @column()
+  public value: number
+
+  @column()
+  public buffDuration: number
+
+  @column()
+  public buffScale: number
+
+  @column()
+  public oppositeScale: number
+
+  @column()
+  public mpGain: number
+
+  @column()
+  public oppositeMpCost: number
+
+  @hasMany(() => Action)
+  public actions: HasMany<typeof Action>
+}
---
+++ api/database/seeders/CreateAspect.ts
@@ -0,0 +1,70 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import { Aspect } from 'App/Models'
+
+export default class CreateAspectSeeder extends BaseSeeder {
+  public async run() {
+    await Aspect.createMany([
+      {
+        'key': 'AF1',
+        'name': 'Astral Fire I',
+        'value': 1,
+        'buffDuration': 15,
+        'buffScale': 1.4,
+        'oppositeScale': 0.9,
+        'mpGain': 0,
+        'oppositeMpCost': 0
+      },
+      {
+        'key': 'AF2',
+        'name': 'Astral Fire II',
+        'value': 2,
+        'buffDuration': 15,
+        'buffScale': 1.6,
+        'oppositeScale': 0.8,
+        'mpGain': 0,
+        'oppositeMpCost': 0
+      },
+      {
+        'key': 'AF3',
+        'name': 'Astral Fire III',
+        'value': 3,
+        'buffDuration': 15,
+        'buffScale': 1.8,
+        'oppositeScale': 0.7,
+        'mpGain': 0,
+        'oppositeMpCost': 0
+      },
+      {
+        'key': 'UI1',
+        'name': 'Umbral Ice I',
+        'value': 1,
+        'buffDuration': 15,
+        'buffScale': 0,
+        'oppositeScale': 0.9,
+        'mpGain': 3200,
+        'oppositeMpCost': 0
+      },
+      {
+        'key': 'UI2',
+        'name': 'Umbral Ice II',
+        'value': 2,
+        'buffDuration': 15,
+        'buffScale': 0,
+        'oppositeScale': 0.8,
+        'mpGain': 4700,
+        'oppositeMpCost': 0
+      },
+      {
+        'key': 'UI3',
+        'name': 'Umbral Ice III',
+        'value': 3,
+        'buffDuration': 15,
+        'buffScale': 0,
+        'oppositeScale': 0.7,
+        'mpGain': 6200,
+        'oppositeMpCost': 0
+      },
+
+    ])
+  }
+}
