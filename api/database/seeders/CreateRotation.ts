9b692e6877b763e51ccb60c292f1b4d84eea0867
Luiz Alex Butkeivicz <luisalexsh@hotmail.com>
2022-02-16 13:10:41 -0300

    update seeder

--- api/database/seeders/CreateRotation.ts
+++ api/database/seeders/CreateRotation.ts
@@ -5,10 +5,10 @@ export default class CreateRotationSeeder extends BaseSeeder {
   public async run() {
     await Rotation.createMany([
       {
-        'key': 'Rotation name',
+        'key': 'Seeder Rotation',
         'level': '32',
         'rotationTypeId': 2,
-        'description': 'Rotation description',
+        'description': 'A seeder rotation for testing and demo purposes',
         'dps': 9999,
         'pps': 3333,
         'private': false
